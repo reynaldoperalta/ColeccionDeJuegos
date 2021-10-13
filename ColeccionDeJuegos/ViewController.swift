226b4ddd6523cd704237283fedc3a70b4cd2a02e
reynaldoperalta <reynaldopema14@gmail.com>
2021-10-13 13:35:31 -0700

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by reynaldo peralta marquez on 10/13/21.
+//  Copyright © 2021 reynaldo peralta marquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
